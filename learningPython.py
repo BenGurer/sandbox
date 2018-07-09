daed487c093231c54cf48609a3e69d793a170fe9
Ben Gurer <bengurer@googlemail.com>
2018-07-09 16:09:19 +0100

    Create learningPython.py
    A script to learn python and try out things

---
+++ learningPython.py
@@ -0,0 +1,7 @@
+# Python script to learn the basics - focusing on data science tasks
+# This should include:
+# Loading data of different types
+# sorting data
+# cleaning data
+# selecting data
+# basic statisical analysis
